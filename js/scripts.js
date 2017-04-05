ec0a5e4e70c74b627e1a0a3f693d94896ef38c2a
Dominic Brown and Kat Gifford <dominic@>
2017-04-05 09:20:13 -0700

    add basic jQuery for button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@
 
 
 //Front End//
+$(document).ready(function() {
+
+  $("#user-roll").click(function(){
+    $("#turn-score").append("turn score");
+  });
+
+  $("#user-hold").click(function(){
+    $("#player-total").append("Hold pressed");
+  });
+
+});
