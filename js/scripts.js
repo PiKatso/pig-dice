d6e3e594825936b8adfe7421df6ea26ced08942d
Dominic Brown and Kat Gifford <kat@>
2017-04-05 09:37:54 -0700

    add random number function and tie it to die roll

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 //Back End//
 
-
+//random die number generator
+var dieRoll = function() {
+  return Math.floor(Math.random() * 6) + 1;
+}
 
 //Front End//
 $(document).ready(function() {
 
   $("#user-roll").click(function(){
-    $("#turn-score").append("turn score");
+    $("#turn-score").append(dieRoll());
   });
 
   $("#user-hold").click(function(){
